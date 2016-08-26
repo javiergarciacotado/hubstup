1c3bf886f372138185eaa9f88b70cc96a512acf2
Javier Garcia <jgarciacot@wiley.com>
2016-08-26 10:13:04 +0100

    Added Header and NotFound pages. Clean code.

--- frontend/src/js/index.js
+++ frontend/src/js/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
-import App from '../components/App';
 
 /* eslint-disable */
 import main from '../scss/_main.scss';
--- frontend/src/js/routes.js
+++ frontend/src/js/routes.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from '../components/App';
+import NotFound from '../components/404';
 
 export default(
-  <Route path="/" component={App} />
+  <Route path="/" name="app" component={App}>
+    <Route path="*" component={NotFound} />
+  </Route>
 );
