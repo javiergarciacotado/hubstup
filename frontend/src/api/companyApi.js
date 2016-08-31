03234fc6af9c58d6b37937e91158bd4bc1188f70
Javier Garcia <jgarciacot@wiley.com>
2016-08-31 15:39:43 +0100

    Added example data for companies Added api to initially mock requests to backend

---
+++ frontend/src/api/companyApi.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import { companies } from '../../test/data/companyData';
+
+export let CompanyApi = {
+  getAllCompanies: function () {
+
+  },
+  getCompanyById: function (id) {
+
+  },
+  saveCompany: function (company) {
+
+  },
+  deleteCompany: function (id) {
+
+  }
+};
